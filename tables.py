24a47eddf998e67cbd188a6e4dfdd4e1c2f1fb89
Stuart Miller <Stuart.j.miller.8182@gmail.com>
2020-02-13 18:53:26 -0600

    Create classification report

--- tables.py
+++ tables.py
@@ -4,7 +4,10 @@ Procedures for creating tables
 
 import pandas as pd
 from pandas import DataFrame, Series
+import numpy as np
 from numpy import ndarray
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 
 def count_values_table(data_series: Series) -> DataFrame:
     """Transform a categorical series into a table of value counts.
@@ -20,4 +23,27 @@ def count_values_table(data_series: Series) -> DataFrame:
 def confusion_matrix(y_true: ndarray, y_pred: ndarray) -> DataFrame:
     """Create a confusion matrix in DataFrame form
     """
+    # attempt coercion to ndarray is not correct type
+    if not isinstance(y_true, np.ndarray):
+        y_true = np.array(y_true)
+    if not isinstance(y_pred, np.ndarray):
+        y_pred = np.array(y_pred)
+        
     return pd.crosstab(y_true, y_pred, rownames=['True'], colnames=['Predicted'], margins=True)
+
+def classification_report(y_test, y_pred) -> None:
+    """Print a basic classification report
+    """
+    acc = accuracy_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    cm = confusion_matrix(y_test, y_pred)
+    
+    print("Classification Report")
+    print('')
+    print('Confusion Matrix')
+    print(cm)
+    print('')
+    print('Accuracy: {:0.2f}%'.format(acc * 100),
+          '\nPrecision: {:0.2f}%'.format(precision * 100),
+          '\nRecall: {:0.2f}%'.format(recall * 100))
