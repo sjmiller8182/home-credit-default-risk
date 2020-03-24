aa0ecbcf42035385f1ab80aea3821d1e7424f513
Stuart Miller <Stuart.j.miller.8182@gmail.com>
2020-03-24 12:37:56 -0500

    Add clustering experimentation Add function to plot tsne representation

---
+++ notebooks/project_code/clustering.py
@@ -0,0 +1,24 @@
+mport matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.manifold import TSNE
+
+def plot_tsne_clusters(data, 
+                       labels, 
+                       perplexity = 30,
+                       s = 0.5, 
+                       figsize = (10,10),
+                       random_state = None):
+    """Plot tsne 2D composition
+    """
+    # calculate tsne
+    tsne = TSNE(random_state = random_state,
+                perplexity = perplexity)
+    tsne_comp = tsne.fit_transform(data)
+    
+    # create plot
+    num_classes = len(np.unique(labels))
+    palette = np.array(sns.color_palette("hls", num_classes))
+    plt.figure(figsize = figsize)
+    plt.scatter(tsne_comp[:, 0], tsne_comp[:, 1],
+                c = palette[labels], s = s);
